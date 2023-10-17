df2e61b4279a5748e9064ca3b353fc677e9994ce
Jonas Mikkelsen <jmikkelsen40@outlook.com>
2023-10-17 12:12:19 +0200

    switch statement

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+switch (daysUntilExpiration) {
+    case 0: Console.WriteLine("Your subscribtion has expired");
+    break;
+    case 1: Console.WriteLine($"Your subscribtion expires within a day!\nRenew now and save {discountPercentage += 20}");
+    break;
+    case <= 5: Console.WriteLine($"Your subscribtion expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage += 10}");
+    break;
+    case <= 10: Console.WriteLine("Your subscribtion will expire soon. Renew now!");
+    break;
+    default: Console.WriteLine();
+    break;
+}

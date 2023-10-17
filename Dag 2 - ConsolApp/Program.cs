2e761a381cd5d68b116866a68c746822676a15e5
Jonas Mikkelsen <jmikkelsen40@outlook.com>
2023-10-17 13:52:15 +0200

    finsih the foreach

--- Dag 2 - ConsolApp/Program.cs
+++ Dag 2 - ConsolApp/Program.cs
@@ -1,18 +1,6 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
-
-// Your code goes here
-
-switch (daysUntilExpiration) {
-    case 0: Console.WriteLine("Your subscribtion has expired");
-    break;
-    case 1: Console.WriteLine($"Your subscribtion expires within a day!\nRenew now and save {discountPercentage += 20}");
-    break;
-    case <= 5: Console.WriteLine($"Your subscribtion expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage += 10}");
-    break;
-    case <= 10: Console.WriteLine("Your subscribtion will expire soon. Renew now!");
-    break;
-    default: Console.WriteLine();
-    break;
+﻿string[] orderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+foreach (string order in orderIDs) {
+    if (order.StartsWith("B")){
+        Console.WriteLine(order);
+    }
 }

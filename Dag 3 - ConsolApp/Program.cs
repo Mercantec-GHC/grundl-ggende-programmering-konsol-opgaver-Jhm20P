0aa6e5a45181c0f7b44a5e50fa4e9724523ef9fb
Jonas Mikkelsen <jmikkelsen40@outlook.com>
2023-10-19 11:51:23 +0200

    finished day 3 guied project

--- Dag 3 - ConsolApp/Program.cs
+++ Dag 3 - ConsolApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[,] nameList = {
+    {"John", "23"},
+    {"Casper", "32"},
+    {"Jenny", "29"},
+    {"Kim", "24"}
+};
+
+
+
+for (int i = 0; i < nameList.GetLength(0); i++){
+    for (int j = 0; j < nameList.GetLength(1); j++){
+        System.Console.WriteLine(nameList[i, j] + " ");
+    }
+    System.Console.WriteLine();
+}
